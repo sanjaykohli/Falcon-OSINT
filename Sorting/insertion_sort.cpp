c42bee7602cdfec6e5445954fda510258d72cda8
sanjaykohli <143174572+sanjaykohli@users.noreply.github.com>
2024-09-16 20:46:18 +0530

    Update insertion_sort.cpp

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 void selection_sort(int arr[], int n)
 {
-    for(int i = 0; i<=n-1; i++){
+    for(int i = 0; i<n; i++){
         int j = i;
         while(j>0 && arr[j] < arr[j-1]){
             int temp = arr[j];
             arr[j] = arr[j-1];
             arr[j-1] = temp;
+            
+            j--;
+
         }
-        j--;
     }
-   
 }
 
 void print_arr(int arr[], int size){
@@ -33,4 +34,4 @@ int main()
     cout << "Sorted array: ";
     print_arr(arr, size);
     return 0;
-}  
+}
